8e8d325e687636882690a988d1cc3d801feda24d
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-01-11 11:12:09 -0800

    Sketch out an initial IR.
    This is by no means final and is likely to change a lot

--- src/Cryptol/Compiler/IR.hs
+++ src/Cryptol/Compiler/IR.hs
@@ -1,3 +1,87 @@
 module Cryptol.Compiler.IR where
 
+data Name = Name String IRType
+
+data IRType =
+    TArray IRType
+  | TStream IRType
+  | TBool
+  | TWord Int
+  | TTuple [IRType]
+
+data IRDecl =
+  IRFun Name [Name] IRExpr
+
+data IRExprF e =
+    IRVar Name
+  | IRCall Name [e]
+  | IRPrim (IRPrim e)
+  | IRIf e e e
+    deriving (Functor,Foldable,Traversable)
+
+newtype IRExpr = IRExpr (IRExprF IRExpr)
+
+data IRPrim e =
+    WordLit Integer Int   -- ^ value, size
+  | BoolLit Bool
+
+  | Add e e
+  | Sub e e
+  | Mul e e
+  | Div e e
+  | Mod e e
+
+  | Tuple [e]
+  | Select e Int
+
+  | Array IRType [e]
+  | IndexIn e e           -- ^ array, index
+    deriving (Functor,Foldable,Traversable)
+
+
+class HasType t where
+  typeOf :: t -> IRType
+
+instance HasType Name where
+  typeOf (Name _ t) = t
+
+instance HasType e => HasType (IRExprF e) where
+  typeOf expr =
+    case expr  of
+      IRVar x     -> typeOf x
+      IRCall f _  -> typeOf f
+      IRPrim p    -> typeOf p
+      IRIf _ x _  -> typeOf x
+
+instance HasType IRExpr where
+  typeOf (IRExpr e) = typeOf e
+
+instance HasType e => HasType (IRPrim e) where
+  typeOf prim =
+    case prim of
+      WordLit _ n -> TWord n
+      BoolLit _   -> TBool
+
+      Add x _ -> typeOf x
+      Sub x _ -> typeOf x
+      Mul x _ -> typeOf x
+      Div x _ -> typeOf x
+      Mod x _ -> typeOf x
+
+      Tuple es -> TTuple (map typeOf es)
+
+      Select e n ->
+        case typeOf e of
+          TTuple ts | t : _ <- drop n ts -> t
+          _ -> error "typeOf: Select" -- ["Malformed Select"]
+
+      Array t _ -> t
+
+      IndexIn arr _ ->
+        case typeOf arr of
+          TArray t  -> t
+          TStream t -> t
+          _         -> error "typeOf: indexIn"
+
+
 
