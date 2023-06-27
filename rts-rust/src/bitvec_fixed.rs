51c282e086984abbfa42569a563db37d74c756b8
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-06-27 15:47:35 -0700

    Simplify appending code

--- rts-rust/src/bitvec_fixed.rs
+++ rts-rust/src/bitvec_fixed.rs
@@ -35,7 +35,7 @@ macro_rules! BitVec {
 
 impl<const W: usize, const L: usize> BitVec<W,L> {
 
-  const SIZE_OK: ()      = assert!(limbs_for_bits(W) == L);
+  const SIZE_OK: ()          = assert!(limbs_for_bits(W) == L);
 
   /// Total number of bits in the underlying type
   pub const BITS: usize      = L * c::Limb::BITS;
@@ -58,32 +58,39 @@ impl<const W: usize, const L: usize> BitVec<W,L> {
     if Self::PAD == 0 { return *v }
     v >> Self::PAD
   }
+}
 
 
-  /// Construct using a Uint from `crypto-bigint`.
-  pub fn from_uint(x: c::Uint<L>) -> Self {
+// -----------------------------------------------------------------------------
+// From
+
+impl<const W: usize, const L: usize> From<&n::BigUint> for BitVec<W,L> {
+
+  fn from(n: &n::BigUint) -> Self {
+    let mut result = < c::Uint<L> >::default();
+    let buf        = result.as_words_mut();
+    for (r,d) in buf.iter_mut().zip(n.iter_u64_digits()) { *r = d }
+    Self::from(result)
+  }
+}
+
+impl<const W: usize, const L: usize> From<c::Uint<L>> for BitVec<W,L> {
+  fn from(x: c::Uint<L>) -> Self {
     let _ = Self::SIZE_OK;
     if Self::PAD == 0 { return BitVec(x) }
     BitVec(x << Self::PAD)
   }
-
-  /// Construct using a u64
-  pub fn from_u64(x: u64) -> Self {
-    Self::from_uint(<c::Uint<L>>::from_u64(x))
-  }
-
-  /// Construct using a BigUint from `num`.
-  pub fn from_integer(n: &n::BigUint) -> Self {
-    let mut result = < c::Uint<L> >::default();
-    let buf = result.as_words_mut();
-
-    for (r,d) in buf.iter_mut().zip(n.iter_u64_digits()) { *r = d }
-
-    Self::from_uint(result)
-  }
-
 }
 
+impl<const W: usize, const L: usize> From<u64> for BitVec<W,L> {
+  fn from(x : u64) -> Self { Self::from(<c::Uint<L>>::from_u64(x)) }
+}
+// -----------------------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------------------
+// Arithmetic
 impl<const W: usize, const L: usize> Add for &BitVec<W,L> {
   type Output = BitVec<W,L>;
 
@@ -108,7 +115,8 @@ impl<const W: usize, const L: usize> Neg for &BitVec<W,L> {
   type Output = BitVec<W,L>;
 
   fn neg(self) -> Self::Output {
-    &Self::Output::from_u64(0) - self
+    let x = BitVec::<W,L>::from(0);
+    &x - self
   }
 }
 
@@ -134,16 +142,20 @@ impl<const W: usize, const L: usize> Div for &BitVec<W,L> {
   }
 }
 
-
 impl<const W: usize, const L: usize> Rem for &BitVec<W,L> {
   type Output = BitVec<W,L>;
 
   fn rem(self, other: Self) -> Self::Output {
     let lhs = self.to_uint();
     let rhs = other.to_uint();
-    Self::Output::from_uint(lhs.wrapping_rem(&rhs))
+    Self::Output::from(lhs.wrapping_rem(&rhs))
   }
 }
+
+
+
+// -----------------------------------------------------------------------------
+// Append
 
 impl<const WU: usize, const LU: usize> BitVec<WU,LU> {
 
@@ -155,89 +167,35 @@ impl<const WU: usize, const LU: usize> BitVec<WU,LU> {
 
     let BitVec(uint_upper) = self;
     let BitVec(uint_lower) = lower_bv;
-    let upper = uint_upper.as_words();
-    let lower = uint_lower.as_words();
+    let upper              = uint_upper.as_words();
+    let lower              = uint_lower.as_words();
     let mut uint_out : c::Uint<LO> = Default::default();
-    let out = uint_out.as_words_mut();
+    let out                = uint_out.as_words_mut();
 
     assert_eq!(LU, upper.len());
     assert_eq!(LL, lower.len());
     assert_eq!(LO, out.len());
 
-    let pad_upper: usize  = LU * c::Limb::BITS - WU;
-    let pad_lower: usize  = LL * c::Limb::BITS - WL;
-    let pad_out:   usize  = LO * c::Limb::BITS - WO;
+    let pad_upper = BitVec::<WU,LU>::PAD;
+    let pad_lower = BitVec::<WL,LL>::PAD;
+    let mid       = LO - LU;
 
-    const fn other_half(this_half: usize) -> usize { c::Limb::BITS - this_half }
+    // First we copy the upper word into the output.
+    if WU > 0 { out[ mid ..].copy_from_slice(upper); }
 
-    let mut out_ix : usize    = 0;  // limb to write into
-    let mut out_v  : c::Word  = 0;  // partial word to write into index
-
-    let mut write_out = |x : c::Word| {
-      if pad_out == 0 {
-        out[out_ix] = x;
-      } else {
-        out[out_ix] = (x << pad_out) | out_v;
-        out_v       = x >> other_half(pad_out);
-      }
-      out_ix += 1;
-    };
-
-    // Complete a partially read word, and produce a new partially read one.
-    // assume: read_have < c::Limb::BITS
-    let read_in = |read_arr:&[c::Word], read_have, read_v, read_ix| {
-      let v = read_arr[read_ix];
-      if read_have == 0 {
-        (v,0)
-      } else {
-        ( (v << read_have) | read_v
-        , v >> other_half(read_have)
-        )
-      }
-    };
-
-    let mut read_v = lower[0] >> pad_lower;
-    let mut have   = other_half(pad_lower);
-    for i in 1 .. LL {
-      let (a,new_read_v) = read_in(lower,have,read_v,i);
-      write_out(a);
-      read_v = new_read_v;
-    }
-
-    let start = upper[0] >> pad_upper;
-    let have2 = other_half(pad_upper);
-
-    if have == c::Limb::BITS {
-      write_out(read_v);
-      read_v = start;
-      have   = have2;
+    if pad_upper == 0 || WU == 0 {
+      if WL > 0 { out[0 .. LL].copy_from_slice(lower); }
     } else {
-      let w   = (start << have) | read_v;
-      let tot = have + have2;
 
-      if tot >= c::Limb::BITS {
-        write_out(w);
-        read_v = start >> other_half(have);
-        have   = tot - c::Limb::BITS;
-      } else {
-        read_v = w;
-        have   = tot;
+      let have      = c::Limb::BITS - pad_upper;
+      let mut out_v = upper[0];
+      for i in (0 .. LL).rev() {
+        let w        = lower[i];
+        out[mid - i] = out_v | (w >> have);
+        out_v        = w << pad_upper;
       }
+      if have > pad_lower { out[0] = out_v; }
     }
-
-    if have == c::Limb::BITS {
-      write_out(read_v);
-      have   = 0;
-      read_v = 0;
-    }
-
-    for i in 1 .. LU {
-      let (a,new_read_v) = read_in(upper,have,read_v,i);
-      write_out(a);
-      read_v = new_read_v;
-    }
-
-    if have > 0 { write_out(read_v) }
 
     BitVec::<WO,LO>(uint_out)
   }
@@ -247,7 +205,6 @@ impl<const WU: usize, const LU: usize> BitVec<WU,LU> {
 #[macro_export]
 macro_rules! append {
   ($W1:expr,$W2:expr,$xs:expr,$ys:expr) => { {
-    const L1: usize = $crate::bitvec_fixed::limbs_for_bits($W1);
     const L2: usize = $crate::bitvec_fixed::limbs_for_bits($W2);
     const W3: usize = $W1 + $W2;
     const L3: usize = $crate::bitvec_fixed::limbs_for_bits(W3);
@@ -264,21 +221,35 @@ macro_rules! append {
 
 #[cfg(test)]
 mod tests {
-  use num::bigint as n;
+  use num::bigint::ToBigUint;
 
   #[test]
   fn test_append() {
-    let v1 : u64 = 0b1;
-    let x1  = <BitVec!(1)>::from_u64(v1);
-    let v5 : u64 = 0b10001;
-    let x5  = <BitVec!(5)>::from_u64(v5);
-    let v64 : u64 = (1 << 63) + 1;
-    let x64 = <BitVec!(64)>::from_u64(v64);
+    let x0  = <BitVec!(0)>::from(0);
 
-    let m = (vy).to_bigint();
+    let v1 : u64 = 0b1;
+    let x1  = <BitVec!(1)>::from(v1);
+
+    let v5 : u64 = 0b10001;
+    let x5  = <BitVec!(5)>::from(v5);
+
+    let v64 : u64 = (1 << 63) + 1;
+    let x64 = <BitVec!(64)>::from(v64);
+
+
 
     // small small
-    assert_eq!(append!(5,1,&x5,&x1), <BitVec!(6)>::from_u64(0b100011));
+    assert_eq!(append!(0,0,&x0,&x0), x0);
+    assert_eq!(append!(0,1,&x0,&x1), x1);
+    assert_eq!(append!(1,0,&x1,&x0), x1);
+    assert_eq!(append!(5,1,&x5,&x1),   <BitVec!(6)>::from(0b100011));
+    assert_eq!(append!(1,5,&x1,&x5),   <BitVec!(6)>::from(0b110001));
+
+    let m = (((v1 as u128) << 64) | (v64 as u128)).to_biguint().unwrap();
+    assert_eq!(append!(1,64,&x1,&x64), <BitVec!(65)>::from(&m));
+
+    let m1 = (((v64 as u128) << 1) | (v1 as u128)).to_biguint().unwrap();
+    assert_eq!(append!(64,1,&x64,&x1), <BitVec!(65)>::from(&m1));
 
   }
 
--- rts-rust/src/main.rs
+++ rts-rust/src/main.rs
@@ -1,11 +1,10 @@
 use cryptol::*;
-use num::bigint as n;
 
 
 // This is is just for testsing stuff out
 fn main() {
-  let a = <BitVec!(63)>::from_u64(1);
-  let b = <BitVec!(1)>::from_u64(1);
+  let a = <BitVec!(63)>::from(1);
+  let b = <BitVec!(1)>::from(1);
   let c = append!(1,63,&b,&a);
   print!("{:?}\n",a.to_uint());
   print!("{:?}\n",b.to_uint());
