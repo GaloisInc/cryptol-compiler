5ad2bc3270a0cea3b5cae994f3ce835f1b3468a2
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2023-06-26 21:46:11 -0700

    checkpoint

--- rts-rust/src/bitvec_fixed.rs
+++ rts-rust/src/bitvec_fixed.rs
@@ -9,8 +9,8 @@ use num::bigint as n;
 /// The unused least significant bits are set to 0.  This representation allows
 /// us to reuse many operations without any additional overhead
 /// (e.g., add, compare, etc).
-#[derive(Debug)]
-pub struct BitVec<const W: usize, const L: usize>(c::Uint<L>);
+#[derive(Debug,PartialEq,Eq)]
+pub struct BitVec<const W: usize, const L: usize>(pub c::Uint<L>);
 
 pub const fn limbs_for_bits(w: usize) -> usize {
   if w == 0 { return 1; }   // crypto_bigint does not support 0 limbs
@@ -25,22 +25,23 @@ macro_rules! BitVec {
   // XXX: we can add extra cases here to handle small bit-vectors
   // (e.g., <= 32)
 
-  ($w:literal) => { $crate::bitvec_fixed::BitVec
+  ($w:expr) => { $crate::bitvec_fixed::BitVec
                        < $w
                        , {$crate::bitvec_fixed::limbs_for_bits($w)}
                        >
   };
 }
 
+
 impl<const W: usize, const L: usize> BitVec<W,L> {
 
   const SIZE_OK: ()      = assert!(limbs_for_bits(W) == L);
 
   /// Total number of bits in the underlying type
-  const BITS: usize      = L * c::Limb::BITS;
+  pub const BITS: usize      = L * c::Limb::BITS;
 
   /// Extra padding space
-  const PAD: usize       = Self::BITS - W;
+  pub const PAD: usize       = Self::BITS - W;
 
   /// Clear up any bits in the least significant position of the full Uint
   pub fn fix_underflow(&mut self) {
@@ -144,6 +145,117 @@ impl<const W: usize, const L: usize> Rem for &BitVec<W,L> {
   }
 }
 
+impl<const WU: usize, const LU: usize> BitVec<WU,LU> {
+
+  pub fn append
+    < const WL: usize, const LL: usize
+    , const WO: usize, const LO: usize
+    >
+    (&self, lower_bv: &BitVec<WL,LL>) -> BitVec<WO,LO> {
+
+    let BitVec(uint_upper) = self;
+    let BitVec(uint_lower) = lower_bv;
+    let upper = uint_upper.as_words();
+    let lower = uint_lower.as_words();
+    let mut uint_out : c::Uint<LO> = Default::default();
+    let out = uint_out.as_words_mut();
+
+    assert_eq!(LU, upper.len());
+    assert_eq!(LL, lower.len());
+    assert_eq!(LO, out.len());
+
+    let pad_upper: usize  = LU * c::Limb::BITS - WU;
+    let pad_lower: usize  = LL * c::Limb::BITS - WL;
+    let pad_out:   usize  = LO * c::Limb::BITS - WO;
+
+    const fn other_half(this_half: usize) -> usize { c::Limb::BITS - this_half }
+
+    let mut out_ix : usize    = 0;  // limb to write into
+    let mut out_v  : c::Word  = 0;  // partial word to write into index
+
+    let mut write_out = |x : c::Word| {
+      if pad_out == 0 {
+        out[out_ix] = x;
+      } else {
+        out[out_ix] = (x << pad_out) | out_v;
+        out_v       = x >> other_half(pad_out);
+      }
+      out_ix += 1;
+    };
+
+    // Complete a partially read word, and produce a new partially read one.
+    // assume: read_have < c::Limb::BITS
+    let read_in = |read_arr:&[c::Word], read_have, read_v, read_ix| {
+      let v = read_arr[read_ix];
+      if read_have == 0 {
+        (v,0)
+      } else {
+        ( (v << read_have) | read_v
+        , v >> other_half(read_have)
+        )
+      }
+    };
+
+    let mut read_v = lower[0] >> pad_lower;
+    let mut have   = other_half(pad_lower);
+    for i in 1 .. LL {
+      let (a,new_read_v) = read_in(lower,have,read_v,i);
+      write_out(a);
+      read_v = new_read_v;
+    }
+
+    let start = upper[0] >> pad_upper;
+    let have2 = other_half(pad_upper);
+
+    if have == c::Limb::BITS {
+      write_out(read_v);
+      read_v = start;
+      have   = have2;
+    } else {
+      let w   = (start << have) | read_v;
+      let tot = have + have2;
+
+      if tot >= c::Limb::BITS {
+        write_out(w);
+        read_v = start >> other_half(have);
+        have   = tot - c::Limb::BITS;
+      } else {
+        read_v = w;
+        have   = tot;
+      }
+    }
+
+    if have == c::Limb::BITS {
+      write_out(read_v);
+      have   = 0;
+      read_v = 0;
+    }
+
+    for i in 1 .. LU {
+      let (a,new_read_v) = read_in(upper,have,read_v,i);
+      write_out(a);
+      read_v = new_read_v;
+    }
+
+    if have > 0 { write_out(read_v) }
+
+    BitVec::<WO,LO>(uint_out)
+  }
+}
+
+
+#[macro_export]
+macro_rules! append {
+  ($W1:expr,$W2:expr,$xs:expr,$ys:expr) => { {
+    const L1: usize = $crate::bitvec_fixed::limbs_for_bits($W1);
+    const L2: usize = $crate::bitvec_fixed::limbs_for_bits($W2);
+    const W3: usize = $W1 + $W2;
+    const L3: usize = $crate::bitvec_fixed::limbs_for_bits(W3);
+    $xs.append::<$W2, L2, W3, L3>($ys)
+  } }
+
+}
+
 
 
 
@@ -152,10 +264,22 @@ impl<const W: usize, const L: usize> Rem for &BitVec<W,L> {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+  use num::bigint as n;
 
   #[test]
-  fn test() {
+  fn test_append() {
+    let v1 : u64 = 0b1;
+    let x1  = <BitVec!(1)>::from_u64(v1);
+    let v5 : u64 = 0b10001;
+    let x5  = <BitVec!(5)>::from_u64(v5);
+    let v64 : u64 = (1 << 63) + 1;
+    let x64 = <BitVec!(64)>::from_u64(v64);
+
+    let m = (vy).to_bigint();
+
+    // small small
+    assert_eq!(append!(5,1,&x5,&x1), <BitVec!(6)>::from_u64(0b100011));
+
   }
 
 }
--- rts-rust/src/main.rs
+++ rts-rust/src/main.rs
@@ -4,10 +4,11 @@ use num::bigint as n;
 
 // This is is just for testsing stuff out
 fn main() {
-  let n = <n::BigUint as num::Num>::from_str_radix("4",16).unwrap();
-  let x = <BitVec!(7)>::from_integer(&n);
-  let y = <BitVec!(7)>::from_u64(3);
-
-  print!("{:?}\n", (&x / &y).to_uint());
+  let a = <BitVec!(63)>::from_u64(1);
+  let b = <BitVec!(1)>::from_u64(1);
+  let c = append!(1,63,&b,&a);
+  print!("{:?}\n",a.to_uint());
+  print!("{:?}\n",b.to_uint());
+  print!("{:?}\n",c.to_uint());
 
 }
