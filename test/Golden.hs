1c2144a8ea4db8fea8692db66ab15414cadf7274
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-11-17 11:43:43 -0800

    Make the one test work again.

--- test/Golden.hs
+++ test/Golden.hs
@@ -6,7 +6,7 @@ import qualified System.IO.Temp as Temp
 import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.Golden as Golden
 
-import qualified Cryptol.Compiler.Rust.Compiler as Compiler
+import qualified Cryptol.Compiler.Driver as Compiler
 import Cryptol.Compiler.Monad (runCryC)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Lazy.UTF8 as BSU8
@@ -20,11 +20,12 @@ tests =
     expected cryPath = cryPath ++ ".expected"
     runTest :: FilePath -> Tasty.TestTree
     runTest cryPath =
-          Golden.goldenVsString cryPath (expected cryPath) (runCompiledSimple cryPath)
+      Golden.goldenVsString cryPath (expected cryPath)
+                                    (runCompiledSimple cryPath)
 
 compileAndRunRust :: [FilePath] -> FilePath -> IO String
 compileAndRunRust cryFiles cratePath =
-  do  runCryC $ Compiler.compileExample cryFiles "test" cratePath
+  do  runCryC cratePath "test" ["Main"] (Compiler.cry2rust cryFiles)
       Process.readCreateProcess (cargoRun cratePath) ""
   where
     cargoRun cp =
