0bdfe7a2d30604780bb822b6631a0922c7c4b3a3
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-11-17 10:31:54 -0800

    Move executable into its own directory, so it doesn't clash with library

---
+++ rts-rust/exe/main.rs
@@ -0,0 +1,28 @@
+
+// This is just for testing stuff out.
+fn main() {
+
+  let nats = cry_rts::stream!
+        { forall = []
+        , element = u64
+        , history = 1
+        , capture = []
+        , init = [0]
+        , step = |this| 1 + this.get_history(0)
+        };
+
+  let s = cry_rts::stream!
+        { forall = [I : [cry_rts::Type, Iterator<Item=u64>, Clone]]
+        , element = u64
+        , history = 2
+        , capture = [ nats: I = nats.clone() ]
+        , init = [0,1]
+        , step = |this| this.get_history(0) + this.get_history(1)
+                                            + this.nats.next()?
+        };
+
+  for (i,x) in s.take(10).enumerate() {
+    println!("fib({}) = {}",i,x)
+  }
+
+}
